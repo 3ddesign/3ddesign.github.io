fdf08df0c8134289e8e2cfae7b102652cc812fe8
Dmitry Scherbakov <3ddesign@bk.ru>
2017-02-09 16:17:50 +0200

    Build Home - industial theme

--- js/common.js
+++ js/common.js
@@ -1,5 +1,16 @@
-$(document).ready(function() {
-
-
-
+$(function($) {
+    var allAccordions = $('.accordion div.data');
+    var allAccordionItems = $('.accordion .accordion-item');
+    $('.accordion > .accordion-item').click(function() {
+        if ($(this).hasClass('open')) {
+            $(this).removeClass('open');
+            $(this).next().slideUp("slow");
+        } else {
+            allAccordions.slideUp("slow");
+            allAccordionItems.removeClass('open');
+            $(this).addClass('open');
+            $(this).next().slideDown("slow");
+            return false;
+        }
+    });
 });
