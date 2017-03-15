1080aba6e6f9fa07c22094c9c3f5372585a5e7ce
Dmitry Scherbakov <3ddesign@bk.ru>
2017-03-15 15:02:28 +0200

    urlaub

--- portfolio/urlaubsgluck/js/common.js
+++ portfolio/urlaubsgluck/js/common.js
@@ -1,8 +1,9 @@
 const button = document.getElementById("search");
 const container = document.getElementById("photogrid");
 
+
 //random value (may add other words in array)
-const arr = ["friends", "wine", "red"];
+const arr = ["friends", "wine", "red", "travel"];
 let value = (arr[Math.round(Math.random() * (arr.length - 1))]);
 
 //request to pixabay
@@ -13,34 +14,42 @@ getUrls = (val) => {
     xhr.open('GET', URL, true);
     xhr.onload = function() {
         let pics = JSON.parse(this.responseText);
+
+        container.innerHTML = "";
         for (let i = 0; i < 7; i++) {
-            a = 'cell' + i;
-            document.getElementById(a).innerHTML = `<figure><img src="${pics.hits[i].webformatURL}" alt="image"><figcaption> ${pics.hits[i].tags.split(',')[0]}</figcaption></figure>`;
+            // a = 'cell' + i;
+            let figure = document.createElement('figure');
+            figure.classList.add("grid-item");
+            figure.innerHTML = `<img src="${pics.hits[i].webformatURL}" alt="image"><figcaption> ${pics.hits[i].tags.split(',')[0]}</figcaption>`;
+            container.appendChild(figure);
         }
+        imagesLoaded(container, () => {
+            let msnry = new Masonry(container, {
+                columnWidth: "img",
+                gutter: 10
+            });
+        });
     };
     xhr.onerror = () => {
         console.log(`Ошибка ${this.status}`);
     };
-
     xhr.send();
 };
 
 //handler for search button
 getUrls(value);
 button.onclick = (e) => {
-    e.preventDefault();
-
     value = document.getElementById("searchfield").value;
-
+    document.getElementById("searchfield").value = "";
     getUrls(value);
+    e.preventDefault();
 };
 
 //Masonry init with imagesLoaded ()
-const containerGr = document.querySelector('.grid');
 
-imagesLoaded(containerGr, () => {
+imagesLoaded(container, () => {
 
-    let msnry = new Masonry(containerGr, {
+    let msnry = new Masonry(container, {
         columnWidth: "img",
         gutter: 10
     });
