d3f3c1b6547dc5e03dff1bedf453e1d689c1aebb
3ddesign <3ddesign@bk.ru>
2017-08-14 18:01:11 +0300

    test

--- 11/Task1/js/common.js
+++
@@ -1,7 +0,0 @@
-var num = prompt("Введите целое число:");
-if (parseInt(num) != num) {
-    alert("Вы ввели не целое число!");
-} else {
-    var degree = prompt("Введите степень:");
-    console.log(extpow.pow(num, degree));
-}
